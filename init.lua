dd8d56836d264bbe3e480f76816bf668e5a832ae
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-02-25 17:17:10 +0100

    Fix not using system clangd on mac
    Incorrect error reporting

--- init.lua
+++ init.lua
@@ -695,6 +695,11 @@ require('lazy').setup({
           },
         },
       }
+      if vim.fn.has 'mac' == 1 then
+        servers['clangd'] = {
+          cmd = { '/usr/bin/clangd' },
+        }
+      end
 
       -- Ensure the servers and tools above are installed
       --
