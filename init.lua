d0940c64c04dfaf49009d2434b2db7badf4ace07
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-02-25 20:01:38 +0100

    Add border to lazy

--- init.lua
+++ init.lua
@@ -1106,6 +1106,9 @@ require('lazy').setup({
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    border = border,
+    backdrop = 10,
+
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
