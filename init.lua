b44d0e6376d49f23537e701d4ff3b44a2b10b645
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-03-05 10:50:29 +0100

    Transparent which-key window

--- init.lua
+++ init.lua
@@ -324,6 +324,12 @@ require('lazy').setup({
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
       delay = 0,
+      win = {
+        -- border = border,
+        wo = {
+          winblend = 20,
+        },
+      },
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
