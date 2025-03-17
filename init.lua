143042113d463bac6b300b2a5d2146c225143c21
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-03-17 08:42:25 +0100

    Use local map function to simplify keymap section of telescope

--- init.lua
+++ init.lua
@@ -482,18 +482,25 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sc', builtin.git_commits, { desc = '[S]earch [C]ommits' })
-      vim.keymap.set('n', '<leader>sbc', builtin.git_bcommits, { desc = '[S]earch [B]uffer [C]ommits' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { desc = '[S]earch ' .. desc })
+      end
+
+      -- stylua: ignore start
+      map('<leader>sh',       builtin.help_tags,    '[H]elp')
+      map('<leader>sk',       builtin.keymaps,      '[K]eymaps')
+      map('<leader>sf',       builtin.find_files,   '[F]iles')
+      map('<leader>ss',       builtin.builtin,      '[S]elect Telescope')
+      map('<leader>sw',       builtin.grep_string,  'current [W]ord')
+      map('<leader>sg',       builtin.live_grep,    'by [G]rep')
+      map('<leader>sd',       builtin.diagnostics,  '[D]iagnostics')
+      map('<leader>sr',       builtin.resume,       '[R]esume')
+      map('<leader>s.',       builtin.oldfiles,     'Recent Files ("." for repeat)')
+      map('<leader>sc',       builtin.git_commits,  '[C]ommits')
+      map('<leader>sbc',      builtin.git_bcommits, '[B]uffer [C]ommits')
+      map('<leader><leader>', builtin.buffers,      '[ ] Find existing buffers')
+      -- stylua: ignore stop
 
       vim.keymap.set('n', '<leader>stc', function()
         builtin.colorscheme { enable_preview = true }
