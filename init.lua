892823626ba22e3bffbd3d79d685e4306ae24076
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-03-24 09:46:52 +0100

    Formatting

--- init.lua
+++ init.lua
@@ -257,13 +257,13 @@ require('lazy').setup({
     config = function()
       require('catppuccin').setup {
         flavour = 'auto', -- latte, frappe, macchiato, mocha
-        background = { -- :h background
+        background = {    -- :h background
           light = 'latte',
           dark = 'mocha',
         },
         transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
       }
       vim.cmd.colorscheme 'catppuccin'
     end,
