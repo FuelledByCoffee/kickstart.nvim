ec33636b1032dfdda911d4ed1db8ea20bcb5321e
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-08-25 10:40:36 +0200

    Formatting

--- init.lua
+++ init.lua
@@ -262,8 +262,8 @@ require('lazy').setup({
           dark = 'mocha',
         },
         transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer     = false, -- shows the '~' characters after the end of buffers
+        term_colors            = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
       }
       vim.cmd.colorscheme 'catppuccin'
     end,
