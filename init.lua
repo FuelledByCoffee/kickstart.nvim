be33ba88f50f51947f9b93415b38bba1db9f9690
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-09-27 16:48:09 +0200

    Use treesitter syntax highlighting for make
    With additional regex highlighting

--- init.lua
+++ init.lua
@@ -1126,11 +1126,11 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
-        disable = { 'tmux', 'make' },
+        disable = { 'tmux' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'make' },
       },
       indent = { enable = true, disable = { 'ruby', 'json' } },
     },
