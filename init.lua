4c787af4769166e0a00d9f57442c596022701019
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-09-28 11:08:10 +0200

    Transparent background for splits

--- init.lua
+++ init.lua
@@ -177,6 +177,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
     -- Your custom highlight commands go here
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', italic = false })
+    vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'Folded', { link = 'Delimiter' })
     vim.api.nvim_set_hl(0, 'signcolumn', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
