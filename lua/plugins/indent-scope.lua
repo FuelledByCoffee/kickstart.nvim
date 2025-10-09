230630069110b9187d4307bd4753b13116a0ad90
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-09 21:10:22 +0200

    Add indent-scope

---
+++ lua/plugins/indent-scope.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-mini/mini.indentscope',
+  version = '*',
+  config = function()
+    require('mini.indentscope').setup {
+      draw = {
+        delay = 10,
+        animation = function(_, _)
+          return 0.5
+        end,
+      },
+      opts = {
+        border = 'both',
+        indent_at_cursor = true,
+        n_lines = 10000,
+        try_as_border = true,
+      },
+      symbol = '│',
+    }
+  end,
+}
