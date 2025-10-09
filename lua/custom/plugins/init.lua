6159bb02387599a7706db2f2ecaed44da89395ac
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-09 08:33:38 +0200

    Floating help

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'nil70n/floating-help',
+  -- optional
+  opts = {
+    border = border,
+    ratio = 0.8,
+  },
+}
