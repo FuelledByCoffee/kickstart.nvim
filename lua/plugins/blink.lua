670e1b3cdb4830fa46e9a70cb7cd7e940758940b
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-09 20:38:58 +0200

    Move plugin configs into their own files

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,89 @@
+return {
+  { -- Autocompletion
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'default',
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+
+        ['<c-j>'] = { 'snippet_forward', 'fallback' },
+        ['<c-k>'] = { 'snippet_backward', 'fallback' },
+
+        ['<c-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+
+        ['<CR>'] = { 'accept', 'fallback' },
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      completion = {
+        ghost_text = {
+          enabled = true,
+          show_with_menu = true,
+          show_without_menu = true,
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+          update_delay_ms = 100,
+        },
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        },
+      },
+      snippets = { preset = 'luasnip' },
+
+      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- which automatically downloads a prebuilt binary when enabled.
+      --
+      -- By default, we use the Lua implementation instead, but you may enable
+      -- the rust implementation via `'prefer_rust_with_warning'`
+      --
+      -- See :h blink-cmp-config-fuzzy for more information
+      fuzzy = { implementation = 'lua' },
+
+      -- Shows a signature help window while you type arguments for a function
+      signature = { enabled = true },
+    },
+  },
+}
---
+++ lua/plugins/mini.lua
@@ -0,0 +1,71 @@
+return {
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    branch = 'main',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- Examples:
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Align/justify text
+      --
+      -- Examples:
+      -- - gaip, - align inner paragraph around ','
+      -- - gA}␣  - align with preview around space until next empty line
+      require('mini.align').setup()
+
+      -- Move lines of text with Alt+motion
+      require('mini.move').setup()
+
+      -- require('mini.animate').setup()
+      require('mini.tabline').setup()
+      -- require('mini.git').setup {
+      --   job = {
+      --     timeout = 30000,
+      --   },
+      --   command = {
+      --     split = 'vertical',
+      --   },
+      --   vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>'),
+      --   vim.keymap.set('n', '<leader>gP', '<cmd>Git push<cr>'),
+      --   -- local rhs = '<Cmd>lua MiniGit.show_at_cursor()<CR>'
+      --   vim.keymap.set({ 'n', 'x' }, '<Leader>gs', function()
+      --     MiniGit.show_at_cursor()
+      --   end, { desc = 'Show at cursor' })
+      -- }
+
+      -- Autmatically add closing paren
+      require('mini.pairs').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+}
