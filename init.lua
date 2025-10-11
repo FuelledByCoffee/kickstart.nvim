d19aca268f7a22bc8f61c347df5cf08730236e2d
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-11 20:47:22 +0200

    Add nordic glyphs to move keymaps

--- init.lua
+++ init.lua
@@ -153,6 +153,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '√',     ':m .+1<CR>==',     { noremap = true, silent = true })
+vim.keymap.set('n', 'ª',     ':m .-2<CR>==',     { noremap = true, silent = true })
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==',     { noremap = true, silent = true })
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==',     { noremap = true, silent = true })
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
