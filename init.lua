0ca2248a20d3223f41e5a611367e7970484983b4
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-11 20:53:28 +0200

    Use <cmd>

--- init.lua
+++ init.lua
@@ -153,12 +153,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '√',     ':m .+1<CR>==',     { noremap = true, silent = true })
-vim.keymap.set('n', 'ª',     ':m .-2<CR>==',     { noremap = true, silent = true })
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==',     { noremap = true, silent = true })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==',     { noremap = true, silent = true })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('n', '√',     '<cmd>m .+1<CR>==',     { noremap = true, silent = true })
+vim.keymap.set('n', 'ª',     '<cmd>m .-2<CR>==',     { noremap = true, silent = true })
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<CR>==',     { noremap = true, silent = true })
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<CR>==',     { noremap = true, silent = true })
+vim.keymap.set('v', '<A-j>', "<cmd>m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('v', '<A-k>', "<cmd>m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- stylua: ignore end
 -- }}}
