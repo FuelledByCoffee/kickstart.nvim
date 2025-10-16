ba3211611ff693783c46b05f7f22b15fe8d79cc5
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-16 13:04:37 +0200

    Add words from the dictionary and thesaurus to the suggestions

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -31,6 +31,7 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      'archie-judd/blink-cmp-words',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -69,6 +70,55 @@ return {
         default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          -- Use the thesaurus source
+          thesaurus = {
+            name = 'blink-cmp-words',
+            module = 'blink-cmp-words.thesaurus',
+            -- All available options
+            opts = {
+              -- A score offset applied to returned items.
+              -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+              score_offset = 0,
+
+              -- Default pointers define the lexical relations listed under each definition,
+              -- see Pointer Symbols below.
+              -- Default is as below ("antonyms", "similar to" and "also see").
+              definition_pointers = { '!', '&', '^' },
+
+              -- The pointers that are considered similar words when using the thesaurus,
+              -- see Pointer Symbols below.
+              -- Default is as below ("similar to", "also see" }
+              similarity_pointers = { '&', '^' },
+
+              -- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
+              -- 2 is similar words of similar words, etc. Increasing this may slow results.
+              similarity_depth = 2,
+            },
+          },
+          -- Use the dictionary source
+          dictionary = {
+            name = 'blink-cmp-words',
+            module = 'blink-cmp-words.dictionary',
+            -- All available options
+            opts = {
+              -- The number of characters required to trigger completion.
+              -- Set this higher if completion is slow, 3 is default.
+              dictionary_search_threshold = 3,
+
+              -- See above
+              score_offset = 0,
+
+              -- See above
+              definition_pointers = { '!', '&', '^' },
+            },
+          },
+        },
+
+        -- Setup completion by filetype
+        per_filetype = {
+          text = { 'dictionary' },
+          markdown = { 'thesaurus' },
+          gitcommit = { 'dictionary' },
         },
       },
       snippets = { preset = 'luasnip' },
