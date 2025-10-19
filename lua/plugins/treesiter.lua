cc861cf053d6160851c83383c57204a91f52a2a9
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-10-19 20:01:01 +0200

    Tweak indent and syntax for make and json

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -12,13 +12,13 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-        disable = { 'tmux' },
+        disable = { 'tmux', 'make' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby', 'make' },
       },
-      indent = { enable = true, disable = { 'ruby', 'json' } },
+      indent = { enable = true, disable = { 'ruby' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
