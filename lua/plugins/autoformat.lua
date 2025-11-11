303bc97965ba1c8da9f7751821f96629d465d77e
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-11-11 17:12:03 +0100

    Use lsp as fallback

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -29,7 +29,7 @@ return {
       lsp_format = 'fallback',
       formatters_by_ft = {
         lua = { 'stylua' },
-        cmake = { 'gersemi' },
+        cmake = { 'gersemi', lsp_format = 'fallback' },
         rust = { 'rustfmt', lsp_format = 'fallback' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
