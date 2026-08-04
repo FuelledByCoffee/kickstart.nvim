5cbf9e286c4ac635764ccdaa75931eca61474c7f
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-04 13:11:24 +0200

    Move colorscheme config to its own file

--- init.lua
+++ init.lua
@@ -343,30 +343,8 @@ vim.api.nvim_create_autocmd('BufRead', {
 
 -- }}}
 
--- [[ ColorScheme ]] {{{
-vim.api.nvim_create_autocmd('ColorScheme', {
-  group = vim.api.nvim_create_augroup('retrobox-overrides', { clear = false }),
-  pattern = 'retrobox', -- colorscheme or list of schemes
-  callback = function()
-    -- Your custom highlight commands go here
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', italic = false })
-    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', italic = false })
-    vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'QuickFixLine', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'Folded', { bg = vim.api.nvim_get_hl(0, { name = 'Special' }).fg, fg = 'black' })
-    vim.api.nvim_set_hl(0, 'Path', { link = 'Identifier' })
-    vim.api.nvim_set_hl(0, 'signcolumn', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'LspReferenceText', { underline = true })
-    vim.api.nvim_set_hl(0, '@lsp.typemod.function.defaultLibrary.lua', { link = 'Special' })
-  end,
-})
-
-vim.cmd.colorscheme 'retrobox'
--- }}}
-
 require 'kickstart.plugins.neo-tree'
 require 'config.lazy'
-
+require 'colorscheme'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 expandtab foldmethod=marker foldlevel=0
