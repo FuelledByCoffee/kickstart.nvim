3d5a2bfead2567336a4c643cc0bb1e102bed651a
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-08 11:03:34 +0200

    feat: Use solid border

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.g.border = 'none'
+vim.g.border = 'solid'
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
