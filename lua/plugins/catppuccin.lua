6bef182e53657c22f3b85bf0bc083435e498dc8e
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-18 11:00:19 +0200

    feat: custom colors for mini statusline

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -19,6 +19,50 @@ return {
         transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+
+        custom_highlights = function(colors)
+          return {
+            -- 1. Customize the Mode blocks (Text color, Background color)
+            MiniStatuslineModeNormal = {
+              fg = colors.mantle,
+              bg = '#129fff',
+              style = { 'bold' },
+            },
+            MiniStatuslineModeInsert = {
+              fg = '#000000',
+              bg = '#75df25',
+              style = { 'bold' },
+            },
+            MiniStatuslineModeVisual = {
+              fg = colors.mantle,
+              bg = colors.mauve,
+              style = { 'bold' },
+            },
+            MiniStatuslineModeReplace = {
+              fg = '#000000',
+              bg = '#ff0000',
+              style = { 'bold' },
+            },
+            MiniStatuslineModeCommand = {
+              fg = colors.mantle,
+              bg = colors.peach,
+              style = { 'bold' },
+            },
+            MiniStatuslineModeOther = { fg = colors.mantle, bg = colors.teal, style = { 'bold' } },
+
+            -- 2. Customize Dev Info (Git branch, Diagnostics)
+            MiniStatuslineDevinfo = { fg = colors.text, bg = colors.surface1 },
+
+            -- 3. Customize File Name section
+            MiniStatuslineFilename = { fg = colors.subtext1, bg = colors.surface0 },
+
+            -- 4. Customize File Info section (Right side details like filetype)
+            MiniStatuslineFileinfo = { fg = colors.text, bg = colors.surface1 },
+
+            -- 5. Customize the inactive statusline state
+            MiniStatuslineInactive = { fg = colors.overlay0, bg = colors.mantle },
+          }
+        end,
       }
       -- vim.cmd.colorscheme 'catppuccin'
     end,
