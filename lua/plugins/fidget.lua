fab17bc0d9ea150527056e4a33fa9bf196e0e07a
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-18 11:35:24 +0200

    feat: notifications for recording macros
    commit 6b712d91072397c24a15d0e325c6de9a872feb55
    Author: Johannes Eckhoff <j.eckhoff@gmail.com>
    Date:   Fri Sep 18 11:33:53 2026 +0200
    
        feat: notification for recording macro
    
    commit 15760153990705ac94b640530ef40e5ed052c895
    Author: Johannes Eckhoff <j.eckhoff@gmail.com>
    Date:   Fri Sep 18 11:02:03 2026 +0200
    
        feat: it shows a notification, but doesn't dismiss it

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -10,4 +10,39 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    local fidget = require 'fidget'
+    fidget.setup(opts)
+
+    -- Handle macro notifications via fidget
+    local macro_group = vim.api.nvim_create_augroup('FidgetMacroFix', { clear = true })
+    local macro_key = 'macro_recording_status' -- Unique identifier key for the macro notification
+
+    vim.api.nvim_create_autocmd('RecordingEnter', {
+      group = macro_group,
+      callback = function()
+        local register = vim.fn.reg_recording()
+        if register ~= '' then
+          -- Send the notification anchored to our custom unique key
+          fidget.notify('Recording', vim.log.levels.INFO, {
+            key = macro_key,
+            annote = string.format('@%s', register),
+            ttl = math.huge, -- Ensure it doesn't close on its own
+          })
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('RecordingLeave', {
+      group = macro_group,
+      callback = function()
+        -- We update the same key but set ttl to 0, which triggers Fidget's exit routine
+        fidget.notify('Recording finished', vim.log.levels.INFO, {
+          key = macro_key,
+          annote = 'Macro',
+          ttl = 0, -- Drops it immediately
+        })
+      end,
+    })
+  end,
 }
