f18dcd6b9efe1269228ce780e77645ac872221cc
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-24 10:25:11 +0200

    fix: description for cclose

--- init.lua
+++ init.lua
@@ -147,7 +147,7 @@ vim.keymap.set('n', '<leader>mm', '<cmd>make!<cr><cmd>cw<cr>', { desc = '[M]ake'
 vim.keymap.set('n', '<leader>mr', '<cmd>make! run<cr>',        { desc = '[M]ake [R]un'   })
 vim.keymap.set('n', '<leader>mt', '<cmd>make! test<cr>',       { desc = '[M]ake [T]est'  })
 vim.keymap.set('n', '<leader>mc', '<cmd>make clean<cr>',       { desc = '[M]ake [C]lean' })
-vim.keymap.set('n', '<leader>cc', '<cmd>cclose<cr>',           { desc = '[C] [C]lose'    })
+vim.keymap.set('n', '<leader>cc', '<cmd>cclose<cr>',           { desc = '[C]close'    })
 
 vim.keymap.set('x', '<tab>',   '>gv', { desc = 'Indent selected text' })
 vim.keymap.set('x', '<s-tab>', '<gv', { desc = 'Unindent selected text' })
